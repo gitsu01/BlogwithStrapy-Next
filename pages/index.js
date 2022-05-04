e5bccb8ac3646885060d0488a7b04b4cc8c327f8
Subir Mondal <sprivacc0@gmail.com>
2022-05-04 20:48:17 +0530

    First Time

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 
 
 export default function Home() {
   return (
     <>
+      <Header /> {/* Header Component */}
 
+      {/* Main Body Code Start */}
+      <main>
+        Home Body
+      </main>
+      {/* Main Body Code Start */}
+
+      <Footer /> {/* Footer Component */}
     </>
   )
 }
